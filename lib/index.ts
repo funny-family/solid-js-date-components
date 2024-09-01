9516815e5659fdc89dad8b4b47ff98537dd6fdf1
funny-family <maksvadik228@gmail.com>
2024-09-01 15:58:28 +0700

    fix "calculateDaysOfMonth" fucntion

--- lib/index.ts
+++ lib/index.ts
@@ -1,13 +1,14 @@
+import { batch } from 'solid-js';
 import { createMutable } from 'solid-js/store';
+
+export var DATE_SYMBOL = Symbol('DATE_SYMBOL') as symbol;
 
 export var calculateStartDate = (
   startDate: Date,
-  dayWeekStart: number
+  dayOfTheWeekIndex: number
 ): Date => {
-  // var startDate = new Date(date.getFullYear(), date.getMonth(), 1);
-
   // Seeking for Day that weak should start with
-  while (startDate.getDay() !== dayWeekStart) {
+  while (startDate.getDay() !== dayOfTheWeekIndex) {
     const day = startDate.getDate();
     startDate.setDate(day - 1);
   }
@@ -15,84 +16,144 @@ export var calculateStartDate = (
   return startDate;
 };
 
-export var createCalendar = (
-  initialTimeValue: number,
-  option?: Map<string, any>
-) => {
-  var date = new Date();
-  // Initially set date to the first day of the month
-  date.setDate(1);
+/**
+ * @see https://stackoverflow.com/questions/46004656/how-to-reliably-implement-next-previous-month
+ */
+export var setPreviousMonthDate = (date: Date) => {
+  var month = date.getMonth();
 
+  // if (month !== 0) {
+  //   date.setMonth(month - 1);
+  // }
+
+  date.setMonth(month - 1);
+
+  return date;
+};
+
+/**
+ * @see https://stackoverflow.com/questions/46004656/how-to-reliably-implement-next-previous-month
+ */
+export var setNextMonthDate = (date: Date) => {
+  var month = date.getMonth();
+
+  if (month !== 11) {
+    date.setMonth(month + 1);
+  }
+
+  // date.setMonth(month + 1);
+
+  return date;
+};
+
+var calculateDay = (date: Date, time: number, index: number) => {
+  date.setTime(time);
+
+  if (index > 0) {
+    date.setDate(date.getDate() + index);
+  }
+
+  return date.getDate();
+};
+
+export var createCalendar = (initialDate: Date, option?: Map<string, any>) => {
   /*
-    0 -> week starts with "Sunday"
-    1 -> week starts with "Monday"
+    0: 'Sunday'
+    1: 'Monday'
+    2: 'Tuesday'
+    3: 'Wednesday'
+    4: 'Thursday'
+    5: 'Friday'
+    6: 'Saturday'
   */
-  var dayWeekStart: number = option?.get('dayWeekStart') || 1;
-  var startDate = calculateStartDate(date, dayWeekStart);
+  var dayOfTheWeekIndex: number = option?.get('dayOfTheWeekIndex') || 0;
+  var date = new Date(
+    initialDate.getFullYear(),
+    initialDate.getMonth(),
+    dayOfTheWeekIndex
+  );
+
+  var monthIndex = initialDate.getMonth();
+
+  var getStartDay = () => {
+    return calculateStartDate(date, dayOfTheWeekIndex);
+  };
+
+  var startDate = calculateStartDate(date, dayOfTheWeekIndex);
   var startDateTime = startDate.getTime();
 
   /* export */
   var possibleDaysInCalendar = 42 as const;
 
-  var calculateDay = (date: Date, time: number, index: number) => {
-    date.setTime(time);
-
-    if (index > 0) {
-      date.setDate(date.getDate() + index);
-    }
-
-    return date.getDate();
-  };
-
-  /* export */
-  var days = createMutable(
-    Array.from({ length: possibleDaysInCalendar }, (_, index) => {
-      // startDate.setTime(startDateTime);
-
-      // if (index > 0) {
-      //   startDate.setDate(startDate.getDate() + index);
-      // }
-
-      // return startDate.getDate();
-
+  var store = createMutable({
+    year: 0,
+    monthIndex,
+    days: Array.from({ length: possibleDaysInCalendar }, (_, index) => {
       return calculateDay(startDate, startDateTime, index);
-    })
-  );
+    }),
+  });
+
+  date.setFullYear(initialDate.getFullYear());
+  date.setMonth(initialDate.getMonth());
+  date.setDate(dayOfTheWeekIndex);
 
   /* export */
-  var calculateMonth = (predicate: () => number) => {
+  var calculateDaysOfMonth = (predicate: () => number) => {
     var timeValue = predicate();
 
     date.setTime(timeValue);
+    date.setDate(dayOfTheWeekIndex);
 
-    var startDate = calculateStartDate(date, dayWeekStart);
+    var startDate = calculateStartDate(date, dayOfTheWeekIndex);
     var startDateTime = startDate.getTime();
 
-    days.forEach((_, index, array) => {
-      // startDate.setTime(startDateTime);
-
-      // if (index > 0) {
-      //   startDate.setDate(startDate.getDate() + index);
-      // }
-
-      // days[index] = startDate.getDate();
-
-      array[index] = calculateDay(startDate, startDateTime, index);
+    batch(() => {
+      store.days.forEach((_, index, array) => {
+        array[index] = calculateDay(startDate, startDateTime, index);
+      });
     });
   };
 
-  var calculateNextMonth = () => {
-    //
+  var calculatePreviousDaysOfMonth = () => {
+    console.log(date);
+
+    // var previousMonthDate = setPreviousMonthDate(date);
+    // calculateDaysOfMonth(() => {
+    //   return previousMonthDate.getTime();
+    // });
+    // date.setMonth(date.getMonth() - 1);
+    // var startDate = calculateStartDate(date, dayWeekStart);
+    // var startDateTime = startDate.getTime();
+    // store.days.forEach((_, index) => {
+    //   store.days[index] = calculateDay(startDate, startDateTime, index);
+    // });
   };
 
-  var calculatePreviousMonth = () => {
-    //
+  var calculateNextDaysOfMonth = () => {
+    console.log(1, date);
+    date.setFullYear(date.getFullYear());
+    setNextMonthDate(date);
+    date.setDate(dayOfTheWeekIndex);
+    console.log(2, date);
+
+    var startDate = calculateStartDate(date, dayOfTheWeekIndex);
+    var startDateTime = startDate.getTime();
+
+    batch(() => {
+      store.days.forEach((_, index, array) => {
+        array[index] = calculateDay(startDate, startDateTime, index);
+      });
+    });
   };
 
   return {
-    // date,
+    [DATE_SYMBOL]: date,
     possibleDaysInCalendar,
-    days: days as Readonly<typeof days>,
-    calculateMonth,
+    days: store.days as Readonly<typeof store.days>,
+    year: store.year as Readonly<typeof store.year>,
+    monthIndex: store.monthIndex as Readonly<typeof store.monthIndex>,
+    calculateDaysOfMonth,
+    calculatePreviousDaysOfMonth,
+    calculateNextDaysOfMonth,
   };
 };
