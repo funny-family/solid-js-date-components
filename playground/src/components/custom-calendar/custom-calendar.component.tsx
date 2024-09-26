821e3088ad8d49a14b70f15d2c8ebfefa9117907
funny-family <maksvadik228@gmail.com>
2024-09-26 12:10:49 +0700

    update

--- playground/src/components/custom-calendar/custom-calendar.component.tsx
+++ playground/src/components/custom-calendar/custom-calendar.component.tsx
@@ -3,6 +3,8 @@ import { Component, For, Index } from 'solid-js';
 // import { createCalendar } from '../../../../lib';
 import { createMonthCalendar } from '../../../../lib/core/create-month-calendar';
 
+const monthFormate = new Intl.DateTimeFormat('en-us', { month: 'long' });
+
 export var CustomCalendar: Component = () => {
   var monthCalendar = createMonthCalendar(new Date());
   window.customMonthCalendar = monthCalendar;
@@ -23,7 +25,10 @@ export var CustomCalendar: Component = () => {
         >
           {'<-'}
         </button>
-        <div class="header__year">year</div>
+        <div class="header__year">{monthCalendar.currentYear}</div>
+        <div class="header__month">
+          {monthFormate.format(monthCalendar.daysOfTheMonth[21].time)}
+        </div>
         <button
           class="header__button"
           type="button"
@@ -47,9 +52,9 @@ export var CustomCalendar: Component = () => {
         </Index>
       </div>
       <div class="month-days custom-calendar-grid">
-        <For each={monthCalendar.days}>
-          {(day) => {
-            return <div class="day">{day().getDate()}</div>;
+        <For each={monthCalendar.daysOfTheMonth}>
+          {(dayOfTheMonth) => {
+            return <div class="day">{dayOfTheMonth.day}</div>;
           }}
         </For>
       </div>
