35c61d4f0b44cd3be902d53c743ee90e51c20a97
funny-family <maksvadik228@gmail.com>
2024-10-21 12:17:20 +0700

    update

--- lib/plugins/with-min-max-date/with-min-max-date.plugin.ts
+++ lib/plugins/with-min-max-date/with-min-max-date.plugin.ts
@@ -1,43 +1,76 @@
 import { SetStoreFunction } from 'solid-js/store';
 import {
   createMonthCalendar,
+  MUTABLE_DATE_SYMBOL,
   SET_STATE_SYMBOL,
 } from '../../core/create-month-calendar';
 
+type MonthCalendar = ReturnType<typeof createMonthCalendar>;
+
+var inRange = (x: number, min: number, max: number) => {
+  return x >= min && x <= max;
+};
+
 export var withMinMaxDate = (
-  monthCalendar: ReturnType<typeof createMonthCalendar>,
-  minDate: Date | number,
-  maxDate: Date | number
+  monthCalendar: MonthCalendar,
+  minDate: Date,
+  maxDate: Date
 ) => {
-  console.log(monthCalendar);
+  var setState: SetStoreFunction<MonthCalendar> = (monthCalendar as any)[
+    SET_STATE_SYMBOL
+  ];
 
-  var setState = (monthCalendar as any)[SET_STATE_SYMBOL] as SetStoreFunction<
-    ReturnType<typeof createMonthCalendar>
-  >;
+  var mutableDate: Date = (monthCalendar as any)[MUTABLE_DATE_SYMBOL];
 
-  console.log(setState);
-
-  var minDateValue = Number(minDate);
-  var maxDateValue = Number(maxDate);
+  var isDateLimit = (date: Date, monthCalendar: MonthCalendar) => {
+    return (
+      date.getMonth() === monthCalendar.monthIndex &&
+      date.getFullYear() === monthCalendar.year
+    );
+  };
 
   var monthCalendar_calculatePreviousMonth =
     monthCalendar.calculatePreviousMonth;
   var calculatePreviousMonth = () => {
-    console.log('calculatePreviousMonth');
+    if (isDateLimit(minDate, monthCalendar)) {
+      return;
+    }
 
     monthCalendar_calculatePreviousMonth();
   };
 
   var monthCalendar_calculateNextMonth = monthCalendar.calculateNextMonth;
   var calculateNextMonth = () => {
-    console.log('calculateNextMonth');
+    if (isDateLimit(maxDate, monthCalendar)) {
+      return;
+    }
 
     monthCalendar_calculateNextMonth();
+  };
+
+  var monthCalendar_calculateMonth = monthCalendar.calculateMonth;
+  var calculateMonth = (predicate: () => number | Date) => {
+    var dateTime = Number(predicate());
+
+    mutableDate.setTime(dateTime);
+
+    var monthIndex = mutableDate.getMonth();
+    var year = mutableDate.getFullYear();
+
+    if (
+      inRange(monthIndex, minDate.getMonth(), maxDate.getMonth()) === false ||
+      inRange(year, minDate.getFullYear(), maxDate.getFullYear()) === false
+    ) {
+      return;
+    }
+
+    monthCalendar_calculateMonth(predicate);
   };
 
   setState((state) => {
     return {
       ...state,
+      calculateMonth,
       calculatePreviousMonth,
       calculateNextMonth,
     };
